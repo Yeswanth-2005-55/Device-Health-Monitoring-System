c8802d61c446a4c7ccdd9d9f97a39a7ad85c6b7b
Yeswanthchandran G <yeswanthchandrang2005@gmail.com>
2025-12-02 14:05:20 +0530

    Add files via upload

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DeviceMonitor
+{
+    class TemperatureModule
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Temperature Module Running...");
+            VibrationModule.RunVibrationCheck();
+        }
+    }
+
+    class VibrationModule
+    {
+        public static void RunVibrationCheck()
+        {
+            Console.WriteLine("Vibration Module Check Executed.");
+        }
+    }
+}
+
